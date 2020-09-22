4ca4390aad7f056ee19ca6f88fd8b253b38b80cf
Ignatius22 <igee54077@gmail.com>
2020-09-23 00:31:10 +0100

    add navbar

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 const getWeatherData = (() => {
   const getWeather = async (city, unit = 'F') => {
-    const key = '3bcff5eb7f5c2ce7a067f51ae1ab8863';
-    const getData = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&APPID=${key}`,
+    const API_KEY = '3bcff5eb7f5c2ce7a067f51ae1ab8863';
+    const getData = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&APPID=${API_KEY}`,
       { mode: 'cors' });
     const weatherData = await getData.json();
     return weatherData;
