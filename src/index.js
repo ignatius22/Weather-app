86e2a767c0844458bba91b80b8d57882fa79efc5
Ignatius22 <igee54077@gmail.com>
2020-09-23 09:56:13 +0100

    add method for toggling unit and and method for reseting the value

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import renderHomepage from './renderPage';
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-confusing-arrow */
 /* eslint-disable no-unused-expressions */
-const controller = ((data, ui) => {
-  
+const controller = ((data, renderHomepage) => {
+  let wD;
+  let units = 'F';
+  const toggleUnits = (units) => (units === 'F' ? 'imperial' : 'metric');
+  const searchVal = document.getElementById('searchInput');
+
+  const resetValue = (searchVal) => {
+    searchVal.value = '';
+  };
+
+  /* eslint-disable consistent-return */
+ 
 })(data, ui);
