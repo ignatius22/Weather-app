b24fc60de3b4f7179c2775932f24caa5642edfa5
Ignatius22 <igee54077@gmail.com>
2020-09-23 17:36:14 +0100

    fix error handling for async/await

--- src/data.js
+++ src/data.js
@@ -1,11 +1,15 @@
 const getWeatherData = (() => {
   const getWeather = async (city, unit = 'F') => {
     const API_KEY = '3bcff5eb7f5c2ce7a067f51ae1ab8863';
-    const URL = 'http://api.openweathermap.org/data/2.5/weather?q';
-    const getData = await fetch(`${URL}=${city}&units=${unit}&APPID=${API_KEY}`,
-      { mode: 'cors' });
-    const weatherData = await getData.json();
-    return weatherData;
+    const URL = 'https://api.openweathermap.org/data/2.5/weather?q';
+
+    try {
+      const getData = await fetch(`${URL}=${city}&units=${unit}&APPID=${API_KEY}`, { mode: 'cors' });
+      const weatherData = await getData.json();
+      return weatherData;
+    } catch (error) {
+      return error;
+    }
   };
   return { getWeather };
 })();
