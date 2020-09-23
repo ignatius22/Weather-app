3df39a092571f96c99f1b7534040adf94da5829d
Ignatius22 <igee54077@gmail.com>
2020-09-23 11:52:19 +0100

    add background image

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import data from './data';
 import renderHomepage from './renderPage';
+import './style.css'
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-confusing-arrow */
 /* eslint-disable no-unused-expressions */
