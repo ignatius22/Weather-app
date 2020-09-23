f5c5816da3c9d4237b16cbf7e765cb2fdf050357
Ignatius22 <igee54077@gmail.com>
2020-09-23 09:53:48 +0100

    add controller module

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import renderHomepage from './renderPage';
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-confusing-arrow */
 /* eslint-disable no-unused-expressions */
-
+const controller = ((data, ui) => {
+  
+})(data, ui);
