65fcda797d006136bbef334b4867289a72f38b51
Ignatius22 <igee54077@gmail.com>
2020-09-23 13:38:41 +0100

    validate eslint

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 const getWeatherData = (() => {
   const getWeather = async (city, unit = 'F') => {
     const API_KEY = '3bcff5eb7f5c2ce7a067f51ae1ab8863';
-    const URL = 'http://api.openweathermap.org/data/2.5/weather?q'
+    const URL = 'http://api.openweathermap.org/data/2.5/weather?q';
     const getData = await fetch(`${URL}=${city}&units=${unit}&APPID=${API_KEY}`,
       { mode: 'cors' });
     const weatherData = await getData.json();
