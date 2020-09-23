e3956f58a7582df141b5eef50dfebe709d2c4de8
Ignatius22 <igee54077@gmail.com>
2020-09-23 13:31:28 +0100

    modify getWeatherApp

--- src/data.js
+++ src/data.js
@@ -1,7 +1,8 @@
 const getWeatherData = (() => {
   const getWeather = async (city, unit = 'F') => {
     const API_KEY = '3bcff5eb7f5c2ce7a067f51ae1ab8863';
-    const getData = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&APPID=${API_KEY}`,
+    const URL = 'http://api.openweathermap.org/data/2.5/weather?q'
+    const getData = await fetch(`${URL}=${city}&units=${unit}&APPID=${API_KEY}`,
       { mode: 'cors' });
     const weatherData = await getData.json();
     return weatherData;
